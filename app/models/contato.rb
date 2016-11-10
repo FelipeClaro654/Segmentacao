4ad493059b3f949298457bb8c8685593c9780644
Felipe <felipe.claro654@gmail.com>
2016-11-10 18:06:23 -0200

    Insere o form de busca com as opções de buscas

--- app/models/contato.rb
+++ app/models/contato.rb
@@ -1,7 +1,12 @@
 class Contato < ApplicationRecord
-  belongs_to :estado
-  validates :nome, presence: true
-  validates :email, uniqueness: true, presence: true
-  validates :idade, presence: true
-  validates :cargo, presence: true
+    belongs_to :estado
+    validates :nome, presence: true
+    validates :email, uniqueness: true, presence: true
+    validates :idade, presence: true
+    validates :cargo, presence: true
+
+    def self.search(params)
+        byebug
+        where("nome LIKE ?", "%#{nome}%")
+    end
 end
